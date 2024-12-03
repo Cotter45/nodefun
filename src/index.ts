a0268e57c97f8fea4e4434e0294419aaf251be44
Sean <sd.cotter45@gmail.com>
2024-12-03 16:48:20 -0500

    fix lint error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createServer } from "./core/server.js";
 
 createServer().then((server) => {
-  server.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-  });
+	server.listen(3000, () => {
+		console.log("Server is running on http://localhost:3000");
+	});
 });
